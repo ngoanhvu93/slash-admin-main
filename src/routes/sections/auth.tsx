4d9319c5d00b78b8c278578e8e7c94ffd872b5c2
Vu Ngo <vu_ngo@fastboy.net>
2025-08-19 16:41:47 +0700

    first commit

---
+++ src/routes/sections/auth.tsx
@@ -0,0 +1,23 @@
+import LoginPage from "@/pages/sys/login";
+import { Suspense } from "react";
+import type { RouteObject } from "react-router";
+import { Outlet } from "react-router";
+
+const authCustom: RouteObject[] = [
+	{
+		path: "login",
+		element: <LoginPage />,
+	},
+];
+
+export const authRoutes: RouteObject[] = [
+	{
+		path: "auth",
+		element: (
+			<Suspense>
+				<Outlet />
+			</Suspense>
+		),
+		children: [...authCustom],
+	},
+];
